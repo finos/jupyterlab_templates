ef7f88f348caba85f0b3b47f962141fc812c59f1
Tim Paine <t.paine154@gmail.com>
2019-06-29 15:14:32 -0400

    partial fix for #33, some open questions still

--- jupyterlab_templates/extension.py
+++ jupyterlab_templates/extension.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import fnmatch
 import json
+import jupyter_core.paths
 from io import open
 from notebook.base.handlers import IPythonHandler
 from notebook.utils import url_path_join
@@ -45,8 +46,8 @@ def load_jupyter_server_extension(nb_server_app):
     host_pattern = '.*$'
     print('Installing jupyterlab_templates handler on path %s' % url_path_join(base_url, 'templates'))
 
-    # TODO
-    # template_dirs.append('STANDARD INSTALL DIR')
+    template_dirs.extend([os.path.join(x, 'notebook_templates') for x in jupyter_core.paths.jupyter_path()])
+    print('Search paths:\n\t%s' % '\n\t'.join(template_dirs))
 
     templates = {}
     for path in template_dirs:
