55c7ecb1e8751072df81749e649e01d6dff9b884
Tim Paine <t.paine154@gmail.com>
2022-12-31 21:37:46 -0500

    fix lint error for new black version

--- jupyterlab_templates/tests/test_extension.py
+++ jupyterlab_templates/tests/test_extension.py
@@ -12,7 +12,6 @@ from jupyterlab_templates.extension import load_jupyter_server_extension
 
 class TestExtension:
     def test_load_jupyter_server_extension(self):
-
         m = MagicMock()
 
         m.web_app.settings = {}
