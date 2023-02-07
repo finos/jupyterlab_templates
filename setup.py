84eb7d8a31758f0565c4fe067de6adf6e6040036
nileshsinha1993 <nilesh.sinha@curefit.com>
2023-02-07 23:48:49 +0530

    Removing deprectaed libs.

--- setup.py
+++ setup.py
@@ -4,21 +4,16 @@ from os import path
 from jupyter_packaging import (
     combine_commands,
     create_cmdclass,
-    ensure_python,
     ensure_targets,
-    get_version,
     install_npm,
 )
 from setuptools import find_packages, setup
 
 pjoin = path.join
 
-ensure_python(("2.7", ">=3.7"))
-
 name = "jupyterlab_templates"
 here = path.abspath(path.dirname(__file__))
 jshere = path.abspath(path.join(path.dirname(__file__), "js"))
-version = get_version(pjoin(here, name, "_version.py"))
 
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read().replace("\r\n", "\n")
@@ -26,11 +21,9 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 requires = ["jupyterlab>=3.0.0"]
 
 dev_requires = requires + [
-    "black>=20.",
-    "awscli",
-    "boto3",
-    "requests",
+    "black>=20.0*",
     "bump2version>=1.0.0",
+    "check-manifest",
     "flake8>=3.7.8",
     "flake8-black>=0.2.1",
     "mock",
@@ -67,22 +60,22 @@ cmdclass["js"] = combine_commands(
 
 setup(
     name=name,
-    version=version,
+    version="0.3.2",
     description="Notebook templates",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jpmorganchase/jupyterlab_templates",
+    url="https://github.com/finos/jupyterlab_templates",
     author="the jupyterlab_templates authors",
     license="Apache 2.0",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
+        "Framework :: Jupyter :: JupyterLab",
     ],
     cmdclass=cmdclass,
     keywords="jupyter jupyterlab",
@@ -95,4 +88,5 @@ setup(
     extras_require={"dev": dev_requires},
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.7",
 )
