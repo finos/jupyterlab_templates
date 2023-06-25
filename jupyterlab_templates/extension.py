2e70389f124ae866a95064faf23686007f6efbb8
Randolf Scholz <randolf.scholz@gmail.com>
2023-06-25 17:53:00 +0200

    added code for user templates

--- jupyterlab_templates/extension.py
+++ jupyterlab_templates/extension.py
@@ -7,11 +7,11 @@
 #
 import fnmatch
 import json
+import jupyter_core.paths
 import os
 import os.path
-import jupyter_core.paths
+import platform
 import tornado.web
-
 from io import open
 from jupyter_server.base.handlers import JupyterHandler
 from jupyter_server.utils import url_path_join
@@ -21,56 +21,95 @@ class TemplatesLoader:
     def __init__(self, template_dirs):
         self.template_dirs = template_dirs
 
+    @staticmethod
+    def get_user_templates_dir():
+        """Get the user templates directory."""
+        detected_os = platform.system()
+
+        if detected_os == "Linux":
+            return os.path.join(os.path.expanduser("~"), "Templates")
+        if detected_os == "Darwin":  # MacOS
+            return os.path.join(os.path.expanduser("~"), "Library", "User Template")
+        if detected_os == "Windows":
+            return os.path.join(os.path.expandvars("%USERPROFILE%"), "Templates")
+
+        raise OSError("Unsupported operating system: %s" % detected_os)
+
+    @staticmethod
+    def get_templates_from_dir(path):
+        """Get all templates from a directory and its subdirectories.
+
+        Returns:
+            template_paths list[{"name" : path}]: List of dictionarys with a single entry
+                "name", which returns the relative path to the template.
+            templates_data dict[relative_path, dict]: dictionary of dictionaries
+                with the relative path as key that holds the template data.
+        """
+        template_paths = []
+        templates_data = {}
+
+        # in order to produce correct filenames, abspath should point to the parent directory of path
+        abspath = os.path.abspath(os.path.join(os.path.realpath(path), os.pardir))
+        files = []
+        # get all files in subdirectories
+        for dirname, _, filenames in os.walk(path, followlinks=True):
+            for filename in fnmatch.filter(filenames, "*.ipynb"):
+                if ".ipynb_checkpoints" not in dirname:
+                    files.append(
+                        (
+                            os.path.join(dirname, filename),
+                            dirname.replace(path, ""),
+                            filename,
+                        )
+                    )
+        # pull contents and push into templates list
+        for f, dirname, filename in files:
+            # skips over faild attempts to read content
+            try:
+                with open(os.path.join(abspath, f), "r", encoding="utf8") as fp:
+                    content = fp.read()
+            except (FileNotFoundError, PermissionError):
+                # Can't read file, skip
+                continue
+
+            data = {
+                "path": f,
+                "name": os.path.join(dirname, filename),
+                "dirname": dirname,
+                "filename": filename,
+                "content": content,
+            }
+            template_paths.append({"name": data["name"]})
+            templates_data[data["name"]] = data
+        return template_paths, templates_data
+
     def get_templates(self):
         templates = {}
         template_by_path = {}
+        # add user templates dir
+        user_templates_dir = self.get_user_templates_dir()
+        if os.path.isdir(user_templates_dir):
+            template_paths, templates_data = self.get_templates_from_dir(
+                user_templates_dir
+            )
+            if template_paths:
+                templates[user_templates_dir] = template_paths
+                template_by_path.update(templates_data)
 
-        for path in self.template_dirs:
-            # in order to produce correct filenames, abspath should point to the parent directory of path
-            abspath = os.path.abspath(os.path.join(os.path.realpath(path), os.pardir))
-            files = []
-            # get all files in subdirectories
-            for dirname, _, filenames in os.walk(path, followlinks=True):
-                if dirname == path:
-                    # Skip top level
-                    continue
+        # add builtin templates
+        for template_folder in self.template_dirs:
+            if not os.path.isdir(template_folder):
+                continue
 
-                for filename in fnmatch.filter(filenames, "*.ipynb"):
-                    if ".ipynb_checkpoints" not in dirname:
-                        files.append(
-                            (
-                                os.path.join(dirname, filename),
-                                dirname.replace(path, ""),
-                                filename,
-                            )
-                        )
-            # pull contents and push into templates list
-            for f, dirname, filename in files:
-                # skips over faild attempts to read content
-                try:
-                    with open(os.path.join(abspath, f), "r", encoding="utf8") as fp:
-                        content = fp.read()
-                except (FileNotFoundError, PermissionError):
-                    # Can't read file, skip
-                    continue
-
-                data = {
-                    "path": f,
-                    "name": os.path.join(dirname, filename),
-                    "dirname": dirname,
-                    "filename": filename,
-                    "content": content,
-                }
-
-                # remove leading slash for select
-                if dirname.strip(os.path.sep) not in templates:
-                    templates[dirname.strip(os.path.sep)] = []
-
-                # don't include content unless necessary
-                templates[dirname.strip(os.path.sep)].append({"name": data["name"]})
-
-                # full data
-                template_by_path[data["name"]] = data
+            folder_name = os.path.basename(template_folder)
+            for item in os.listdir(template_folder):
+                path = os.path.join(template_folder, item)
+                if os.path.isdir(path):
+                    template_paths, templates_data = self.get_templates_from_dir(path)
+                    if template_paths:
+                        name = os.path.join(folder_name, item)
+                        templates[name] = template_paths
+                        template_by_path.update(templates_data)
 
         return templates, template_by_path
 
