91691e4206b87759d3d34d8f8fcce7a04d28dabb
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-02 16:57:29 -0400

    Fix merge conflict in test_all.py
    Removed merge conflict markers from test_all.py

--- jupyterlab_templates/tests/test_all.py
+++ jupyterlab_templates/tests/test_all.py
@@ -1,4 +1,3 @@
-<<<<<<< before updating
 # *****************************************************************************
 #
 # Copyright (c) 2020, the jupyterlab_templates authors.
@@ -6,8 +5,6 @@
 # This file is part of the jupyterlab_templates library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
-=======
->>>>>>> after updating
 from jupyterlab_templates import *
 
 
